c940bd6bad603f685834ff4d7fd2096af71f1aac
Kyle Dunaway <kdunaway1@mail.csuchico.edu>
2013-10-08 15:47:53 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RideShare::Application.config.secret_token = '25f51677f028edb96d63f734fa7a76531d16fea56805d6a446325dc1e789f846f087f20ed261212f96895b857eebeb2b87d5dd4643b0f05fe0719ea8cd8f82f4'
